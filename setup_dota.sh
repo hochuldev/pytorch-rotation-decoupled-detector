d8ac16da2eff1244c3cecb593ca13fc16eb7f856
Hochul Song <hocsong@cx2r8s4.42seoul.kr>
2023-07-16 14:18:00 +0900

    불필요한 라인 삭제

--- setup_dota.sh
+++ setup_dota.sh
@@ -5,7 +5,6 @@ apt-get install python3-pip
 apt-get install unzip
 
 git clone https://github.com/circulosmeos/gdown.pl.git
-git clone https://github.com/hochuldev/pytorch-rotation-decoupled-detector.git
 
 ./install_dota_dataset.sh
 
