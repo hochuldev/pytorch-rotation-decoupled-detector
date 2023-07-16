febfeadc1002c3f14ae364b2f8f53b4988a682e6
Hochul Song <hocsong@cx2r8s4.42seoul.kr>
2023-07-16 14:12:24 +0900

    requirements.txt의 패키지를 설치하고 rbbox_overlap_gpu setup.py를 실행하여 추가 설정함

--- setup_dota.sh
+++ setup_dota.sh
@@ -10,3 +10,7 @@ git clone https://github.com/hochuldev/pytorch-rotation-decoupled-detector.git
 ./install_dota_dataset.sh
 
 ./setup_weights.sh
+
+pip install -r pytorch-rotation-decoupled-detector/requirements.txt
+
+python3.8 pytorch-rotation-decoupled-detector/utils/box/ext/rbbox_overlap_gpu setup.py build_ext --inplace
