bedbb9250cb63dcbc4d5a75d02b1f8b07dd22730
Hochul Song <hocsong@cx2r8s4.42seoul.kr>
2023-07-16 14:21:18 +0900

    스크립트 오류 개선

--- install_dota_dataset.sh
+++ install_dota_dataset.sh
@@ -7,13 +7,13 @@ mkdir -p pytorch-rotation-decoupled-detector/images/test
 mkdir -p pytorch-rotation-decoupled-detector/labelTxt/train
 mkdir -p pytorch-rotation-decoupled-detector/labelTxt/val
 
-gdown/gdown.pl https://drive.google.com/file/d/1BlaGYNNEKGmT6OjZjsJ8HoUYrTTmFcO2/view?usp=drive_link pytorch-rotation-decoupled-detector/images/train/part1.zip
-gdown/gdown.pl https://drive.google.com/file/d/1JBWCHdyZOd9ULX0ng5C9haAt3FMPXa3v/view?usp=drive_link pytorch-rotation-decoupled-detector/images/train/part2.zip
-gdown/gdown.pl https://drive.google.com/file/d/1pEmwJtugIWhiwgBqOtplNUtTG2T454zn/view?usp=drive_link pytorch-rotation-decoupled-detector/images/train/part3.zip
-gdown/gdown.pl https://drive.google.com/file/d/1I-faCP-DOxf6mxcjUTc8mYVPqUgSQxx6/view?usp=drive_link pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip
+gdown.pl/gdown.pl https://drive.google.com/file/d/1BlaGYNNEKGmT6OjZjsJ8HoUYrTTmFcO2/view?usp=drive_link pytorch-rotation-decoupled-detector/images/train/part1.zip
+gdown.pl/gdown.pl https://drive.google.com/file/d/1JBWCHdyZOd9ULX0ng5C9haAt3FMPXa3v/view?usp=drive_link pytorch-rotation-decoupled-detector/images/train/part2.zip
+gdown.pl/gdown.pl https://drive.google.com/file/d/1pEmwJtugIWhiwgBqOtplNUtTG2T454zn/view?usp=drive_link pytorch-rotation-decoupled-detector/images/train/part3.zip
+gdown.pl/gdown.pl https://drive.google.com/file/d/1I-faCP-DOxf6mxcjUTc8mYVPqUgSQxx6/view?usp=drive_link pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip
 
-gdown/gdown.pl https://drive.google.com/file/d/1uCCCFhFQOJLfjBpcL5MC0DHJ9lgOaXWP/view?usp=drive_link pytorch-rotation-decoupled-detector/images/val/part1.zip
-gdown/gdown.pl https://drive.google.com/file/d/1uFwxA4B7H8zcI1oD11bj0U8z88qroMlG/view?usp=drive_link pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip
+gdown.pl/gdown.pl https://drive.google.com/file/d/1uCCCFhFQOJLfjBpcL5MC0DHJ9lgOaXWP/view?usp=drive_link pytorch-rotation-decoupled-detector/images/val/part1.zip
+gdown.pl/gdown.pl https://drive.google.com/file/d/1uFwxA4B7H8zcI1oD11bj0U8z88qroMlG/view?usp=drive_link pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip
 
 mkdir pytorch-rotation-decoupled-detector/images/train/images1
 mkdir pytorch-rotation-decoupled-detector/images/train/images2
@@ -24,9 +24,9 @@ unzip pytorch-rotation-decoupled-detector/images/train/part2.zip -d pytorch-rota
 unzip pytorch-rotation-decoupled-detector/images/train/part3.zip -d pytorch-rotation-decoupled-detector/images/train/images3
 
 #TODO: images1, images2, images3 안에 사진이 바로 들어간다고 가정한다. images1안에 폴더가 더 있을수도 있으니까 추후 수정.
-mv unzip pytorch-rotation-decoupled-detector/images/train/images1/* pytorch-rotation-decoupled-detector/images/train/ 
-mv unzip pytorch-rotation-decoupled-detector/images/train/images2/* pytorch-rotation-decoupled-detector/images/train/
-mv unzip pytorch-rotation-decoupled-detector/images/train/images3/* pytorch-rotation-decoupled-detector/images/train/
+mv pytorch-rotation-decoupled-detector/images/train/images1/* pytorch-rotation-decoupled-detector/images/train/ 
+mv pytorch-rotation-decoupled-detector/images/train/images2/* pytorch-rotation-decoupled-detector/images/train/
+mv pytorch-rotation-decoupled-detector/images/train/images3/* pytorch-rotation-decoupled-detector/images/train/
 
 #TODO: 위 TODO와 동일
 unzip pytorch-rotation-decoupled-detector/images/val/part1.zip -d pytorch-rotation-decoupled-detector/images/val/
--- setup_weights.sh
+++ setup_weights.sh
@@ -3,5 +3,5 @@
 mkdir -p pytorch-rotation-decoupled-detector/backbone_weights
 mkdir -p pytorch-rotation-decoupled-detector/save/weight
 
-gdown/gdown.pl https://drive.google.com/file/d/1X-2k1KqG8sJpc7I_yY9hBP9LQzpVVMT2/view?usp=drive_link pytorch-rotation-decoupled-detector/backbone_weights/resnet101-5d3b4d8f.pth
-gdwon/gdown.pl https://drive.google.com/file/d/1iT8R5wQdr4OxnkGykUKogGHEhElFnEp3/view?usp=drive_link pytorch-rotation-decoupled-detector/save/weight/250000.pth
+gdown.pl/gdown.pl https://drive.google.com/file/d/1X-2k1KqG8sJpc7I_yY9hBP9LQzpVVMT2/view?usp=drive_link pytorch-rotation-decoupled-detector/backbone_weights/resnet101-5d3b4d8f.pth
+gdwon.pl/gdown.pl https://drive.google.com/file/d/1iT8R5wQdr4OxnkGykUKogGHEhElFnEp3/view?usp=drive_link pytorch-rotation-decoupled-detector/save/weight/250000.pth
